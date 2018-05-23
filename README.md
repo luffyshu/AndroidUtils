544aa4f1107d0ea68b8fd251eb30763acd8ec51c
Luffy Shu <shuliufei@ehousechina.com>
2018-05-23 15:06:30 +0800

    modify README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # AndroidUtils
+
+## 使用方法
+
+```
+dependencies {
+	implementation 'com.github.luffyshu:AndroidUtils:v1.0.0'
+}
+```
+
--- app/src/main/java/com/luffy/androidutils/MainActivity.java
+++ app/src/main/java/com/luffy/androidutils/MainActivity.java
@@ -3,6 +3,7 @@ package com.luffy.androidutils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.luffy.androidutils.library.MD5Utils;
 import com.luffy.androidutils.library.TimeUtils;
 
 public class MainActivity extends AppCompatActivity {
